fb624a68af7e6d41119cf29e88ca3019ac160fcf
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-03-17 18:16:40 +0100

    toDoList component is created

--- projectAngular/src/app/app.component.ts
+++ projectAngular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello World!!!';
+  subtitle = 'This is my first Angular2 App :)'
 }
---
+++ projectAngular/src/app/todolist/todolist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+  title = 'Lista rzeczy do zrobienia'
+  newTodoTitle: string = ''
+}
