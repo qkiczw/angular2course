91ea4c97f148d0a54f5839da5f5cb4da2569da5e
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-03-20 21:15:50 +0100

    Second Component with random Quotes is added

--- angular2odc3/app.js
+++ angular2odc3/app.js
@@ -5,9 +5,21 @@
     var BrowserModule = ng.platformBrowser.BrowserModule;
     var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic();
 
+    var SecondComponent = Component({
+        selector: 'second',
+        template: '<p><em>{{quote.cytat}}</em></p><small>{{quote.autor}}</small>'
+    })
+    .Class({
+        constructor: function(){
+            var randomIndex = Math.floor(Math.random() * quotes.length);
+            this.quote = quotes[randomIndex];
+        },
+        
+    })
+
     var AppComponent = Component({
         selector: 'my-app',
-        template: '<h1>Angular 2.0 - Hello World!!!</h1>'
+        template: `<h1>Angular 2.0 - Hello World!!!</h1> <second></second>`
     })
     .Class({
         constructor: function(){}
@@ -15,11 +27,17 @@
 
     var AppModule = NgModule({
         imports: [BrowserModule],
-        declarations: [AppComponent],
+        declarations: [AppComponent, SecondComponent],
         bootstrap: [AppComponent]
     })
     .Class({
         constructor: function(){}
     })
     platformBrowserDynamic.bootstrapModule(AppModule);
+
+    var quotes = [
+        {cytat: 'To, że milczę, nie znaczy, że nie mam nic do powiedzenia.', autor: 'Johnatan Caroll'},
+        {cytat: 'Lepiej zaliczać się do niektórych, niż do wszystkich.', autor: 'Andrzej Sapkowski'},
+        {cytat: 'Czytanie książek to najpiękniejsza zabawa, jaką sobie ludzkość wymyśliła.', autor: 'Wisława Szymborska'},
+    ]
 })();
