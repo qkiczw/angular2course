4d50ce7fc72e1d0a0b3b6406f2098f4106c11f71
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-04-24 19:19:59 +0200

    two way binding added and update method removed

--- Angular2_converter/src/app.component.ts
+++ Angular2_converter/src/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'converter',
-  template: `Przelicz: <input id="baseAmountText" type="number" [value]="baseAmount" 
-            (input)="update($event.target.value)"> USD
+  template: `Przelicz: <input id="baseAmountText" type="number" [(ngModel)]="baseAmount"> USD
             <p>
               <strong>{{baseAmount}}</strong> USD = 
               <strong>{{targetAmount}}</strong> GBP 
@@ -19,10 +18,6 @@ export class AppComponent {
 
   exchangeRate = 0.70;
   baseAmount = 1;
-
-  update(baseAmount){
-    this.baseAmount = parseFloat(baseAmount);
-  };
   
   get targetAmount(){
     return this.baseAmount * this.exchangeRate;
