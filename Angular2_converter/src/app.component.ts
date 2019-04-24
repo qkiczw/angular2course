4ae39d15aead17172c83fa18bab9069941a4431a
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-04-24 19:42:56 +0200

    I added isValid function to add class to input if is empty

--- Angular2_converter/src/app.component.ts
+++ Angular2_converter/src/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'converter',
-  template: `Przelicz: <input id="baseAmountText" type="number" [(ngModel)]="baseAmount"> USD
-            <p>
-              <strong>{{baseAmount}}</strong> USD = 
-              <strong>{{targetAmount}}</strong> GBP 
-            </p>
+  template: `<input type="number" [(ngModel)]="baseAmount" [class.error]="isValid(baseAmount)"> USD
+            = <strong>{{targetAmount}}</strong> GBP 
             `,
   styles: [`input[type=number] {
               width: 10ex; text-align: right;
-            }`
+            }
+            .error{
+              background-color: #ff5555;
+            }
+            `
 
   ]
 })
@@ -20,6 +21,11 @@ export class AppComponent {
   baseAmount = 1;
   
   get targetAmount(){
+    console.info(Number.isFinite(this.baseAmount))
     return this.baseAmount * this.exchangeRate;
   };
+
+  isValid(value){
+    return !Number.isFinite(this.baseAmount)
+  }
 }
